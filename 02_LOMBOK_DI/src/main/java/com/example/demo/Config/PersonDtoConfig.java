4bbb6bbfa60271f72415b0aae61b5b29b3340b22
KHS236 <fdsa5104@gmail.com>
2025-09-02 17:13:50 +0900

    복사해서 올리자

---
+++ 02_LOMBOK_DI/src/main/java/com/example/demo/Config/PersonDtoConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.Config;
+
+import com.example.demo.Dto.PersonDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersonDtoConfig {
+
+    @Bean
+    public PersonDto person03(){
+        return PersonDto.builder()
+                .name("고뭉치")
+                .addr("서울")
+                .age(25)
+                .build();
+    }
+    @Bean(name = "personBean")
+    public PersonDto person04() {
+        return PersonDto.builder()
+                .name("괴물")
+                .addr("등장")
+                .age(30)
+                .build();
+    }
+}
